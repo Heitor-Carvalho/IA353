82e6f2a326df4674712a3e9a6008d855fef36e25
Heitor Carvalho <hcarvalho@padtec.com.br>
2017-04-03 16:28:57 -0300

    Replace Jacobian calculation in LM method
    - Replace the Jacobian calculation in the LM method
    by backpropagation function
    
    Change-Id: Ifb3ef6085e0288b92b66cddacaa98f529d31b49a

--- TrainingMethods/batch_lm_training.m
+++ TrainingMethods/batch_lm_training.m
@@ -1,7 +1,6 @@
 function [nn_, err_hist, it] = batch_lm_training(train_set, target, nn, train_par)
   % To do: add support to multiples outputs
   % To do: add function description
-  % Levenberg-Marquart  
   
   nn_ = nn;
   mse_error = train_par.max_error;
@@ -24,25 +23,11 @@ function [nn_, err_hist, it] = batch_lm_training(train_set, target, nn, train_pa
     % Neuro network error
     error = target - nn_out;
     error = reshape(error, 1 , 1, samples_sz);
-    keyboard    
+
     mse_error = mean(error.^2);
       
-    % Output layer weights gradient
-    derror_dw = -mid_layer_func_out_bias;
-    
-    % Middle layer weights gradient
-    w = repmat(nn_.w(:, 2:end), 1, 1, samples_sz);
-    
-    
-    derror_dv = -  repmat(w, nn_.in_sz+1, 1)                                         .* ...
-                   repmat(nn_.diff(mid_layer_func_in), nn_.in_sz+1, 1)               .* ...
-                   repmat(in_bias, 1, nn_.mid_sz);
-
-    Jv = reshape(derror_dv, (nn_.in_sz+1)*nn_.mid_sz, samples_sz)';
-    Jw = reshape(derror_dw, nn_.mid_sz+1, samples_sz)';
-
-    J = [Jv Jw];
-    
+    [~, J] = back_prop_batch_gradient(train_set, target, nn_);
+  
     % Training method Levenberg-Marquart
     d2J = 2*pinv(J'*J + train_par.mu*eye(size(J,2)));
     deltaW = d2J*J'*reshape(error, samples_sz, 1);
