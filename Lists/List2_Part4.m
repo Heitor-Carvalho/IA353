0a9722ebc07e9cb40eda82536c8f610bd79d7052
Heitor Carvalho <hcarvalho@padtec.com.br>
2017-04-28 07:03:45 -0300

    Adding list code to plot figures

--- Lists/List2_Part4.m
+++ Lists/List2_Part4.m
@@ -2,8 +2,19 @@ addpath('../RadialBasisNetwork/')
 addpath('../NeuralNetwork/')
 addpath('./ThirdParty/')
 
+%%
 % Loading training data
 load('channel_training_data.mat')
+
+% Loading channels 
+
+% Case 1
+h_case1 = [0.9 1];                          % Channel
+d_case1 = 0;                                % Delay
+
+% Case 1
+h_case2 = [1 0.5 0.2];                      % Channel
+d_case2 = 2;                                % Delay
 
 % RBF training - Case 1 - Exact number of centrers 
 nn_case1_1 = calc_rbf_network(channel_samples_case1.entrada_treinamento, channel_samples_case1.desejado_treinamento, 8, 0);
@@ -11,25 +22,65 @@ nn_case1_1 = calc_rbf_network(channel_samples_case1.entrada_treinamento, channel
 % RBF training - Case 2 - 4 extra centers
 nn_case1_2 = calc_rbf_network(channel_samples_case1.entrada_treinamento, channel_samples_case1.desejado_treinamento, 12, 0);
 
-% Case 1
-h_case1 = [0.9 1]                           % Channel
-d_case1 = 0;                                % Delay
-
-% Case 1
-h_case2 = [1 0.5 0.2]                       % Channel
-d_case2 = 2;                                % Delay
-
-% Ploting RBF founded centers:
-figure(1)
-plot(nn_case1_1.c(1,:), nn_case1_1.c(2,:),'o')
-grid
-figure(2)
-plot(nn_case1_2.c(1,:), nn_case1_2.c(2,:),'o')
-grid
-
-% Generating map using ThirdParty code
 map_fronteira_mlp_equalizacao_rbf(h_case1, d_case1, nn_case1_1.v, nn_case1_1.w, nn_case1_1.c, nn_case1_1.sig, channel_samples_case1.entrada_treinamento);
 map_fronteira_mlp_equalizacao_rbf(h_case1, d_case1, nn_case1_2.v, nn_case1_2.w, nn_case1_2.c, nn_case1_2.sig, channel_samples_case1.entrada_treinamento);
 
+%% Loading ideal 8 center network
 
+% Loading networks
+nn_struct = load('neural_net_8centers_ideal')
+nn_ideal = nn_struct.nn_case1_1;
 
+% Generating mapping region
+map_fronteira_mlp_equalizacao_rbf(h_case1, d_case1, nn_ideal.v, nn_ideal.w, nn_ideal.c, nn_ideal.sig, channel_samples_case1.entrada_treinamento);
+
+% Generating error graph
+ideal_8center_net_output = neural_nete_rbf(channel_samples_case1.entrada_teste(:, 1:1000), nn_ideal);
+
+% BER and MSE calculation
+ideal_8center_net_output_bpsk = ideal_8center_net_output;
+ideal_8center_net_output_bpsk(ideal_8center_net_output > 0) = 1; 
+ideal_8center_net_output_bpsk(ideal_8center_net_output < 0) = -1; 
+
+mse_ideal_8center = mean((channel_samples_case1.desejado_teste(:, 1:1000) - ideal_8center_net_output).^2);
+ber_ideal_8center = sum(ideal_8center_net_output_bpsk ~= channel_samples_case1.desejado_teste(:, 1:1000))/length(ideal_8center_net_output_bpsk);
+
+%% Loading problem 8 centers network
+
+% Loading networks
+nn_struct = load('neural_net_8centers_problem')
+nn_problem = nn_struct.nn_case1_1;
+
+% Generating mapping region
+map_fronteira_mlp_equalizacao_rbf(h_case1, d_case1, nn_problem.v, nn_problem.w, nn_problem.c, nn_problem.sig, channel_samples_case1.entrada_treinamento);
+
+% Generating error graph
+problem_8center_net_output = neural_nete_rbf(channel_samples_case1.entrada_teste(:, 1:1000), nn_problem);
+
+% BER and MSE calculation
+problem_8center_net_output_bpsk = problem_8center_net_output;
+problem_8center_net_output_bpsk(problem_8center_net_output > 0) = 1; 
+problem_8center_net_output_bpsk(problem_8center_net_output < 0) = -1; 
+
+mse_problem_8center = mean((channel_samples_case1.desejado_teste(:, 1:1000) - problem_8center_net_output).^2);
+ber_problem_8center = sum(problem_8center_net_output_bpsk ~= channel_samples_case1.desejado_teste(:, 1:1000))/length(problem_8center_net_output);
+
+%% Loading problem 12 centers network
+
+% Loading networks
+nn_struct = load('neural_net_12centers')
+nn_12_center = nn_struct.nn_case1_2;
+
+% Generating mapping region
+map_fronteira_mlp_equalizacao_rbf(h_case1, d_case1, nn_12_center.v, nn_12_center.w, nn_12_center.c, nn_12_center.sig, channel_samples_case1.entrada_treinamento);
+
+% Generating error graph
+center12_net_output = neural_nete_rbf(channel_samples_case1.entrada_teste(:, 1:1000), nn_12_center);
+
+% BER and MSE calculation
+center12_net_output_bpsk = center12_net_output;
+center12_net_output_bpsk(center12_net_output > 0) = 1; 
+center12_net_output_bpsk(center12_net_output < 0) = -1; 
+
+mse_12center = mean((channel_samples_case1.desejado_teste(:, 1:1000) - center12_net_output).^2);
+ber_12center = sum(center12_net_output_bpsk ~= channel_samples_case1.desejado_teste(:, 1:1000))/length(center12_net_output_bpsk);
